93ee9469939a2acdfb06e8f5585275d918431710
Shakeitha  Marshall <shakeithamarshall@yahoo.com>
2024-11-12 16:59:08 -0600

    Adding Classes 11/6/2024 Need to Add Category class.

---
+++ Data/Interfaces/InterfaceDogsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Pet_Web_Application_10._12._24_F.Data.Model;
+
+namespace Pet_Web_Application_10._12._24_F.Data.Interfaces
+{
+    public interface InterfaceDogsRepository
+    {
+        IEnumerable<Dog> Dogs { get; set; }
+        IEnumerable<Dog> PrefferedDog { get; set; }
+        Product GetDogById(int dogId);
+    }
+}
