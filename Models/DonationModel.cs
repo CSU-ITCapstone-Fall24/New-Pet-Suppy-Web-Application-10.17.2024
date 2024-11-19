8ba26123daa5546cef97f6e134770d7fbfca5391
Shakeitha  Marshall <shakeithamarshall@yahoo.com>
2024-11-19 17:01:38 -0600

    Working on Donation Page @ 5:01

---
+++ Models/DonationModel.cs
@@ -0,0 +1,12 @@
+﻿using Pet_Web_Application_10._12._24_F.Models;
+
+
+namespace Pet_Web_Application_10._12._24_F.Models
+{
+    public class DonationModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+    }
+}
